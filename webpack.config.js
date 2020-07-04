39249ce9eca1a6d8f0a0f380056e84e4af730a8c
mohur <m.ashrahfi@gmail.com>
2020-07-04 13:24:43 -0700

    V2

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
             exclude: /node_modules/
     }, {
         test: /\.s?css$/,
+        include: /styles/,
         use: [
             'style-loader',
             'css-loader',
