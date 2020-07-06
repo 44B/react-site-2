1ff0f1c498fe829bf97f7805f93b179097719391
mohur <m.ashrahfi@gmail.com>
2020-07-06 15:53:33 -0700

    setting localStorage and some cart actions

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import './styles/styles.scss';
 
-const className = (className) => { return document.querySelector(className)}
+const className = (className) => { return document.querySelector(className); }
 const container = className('single-product-list');
 const img = className('.single-product-img');
 const name = className('single-product-name');
 const price = className('single-product-price');
 
-export const products = {
+const id = (id) => { return document.getElementById(id); }
+
+
+const products = {
     1: {
         name : "Chocolate Chip Cookie",
         img : "https://greedyeats.com/wp-content/uploads/2019/01/Super-thick-walnut-chocolate-chip-cookies.png",
@@ -39,17 +42,54 @@ export const products = {
     }
 };
 
-let size = Object.keys(products).length;
+let cart = {
+    data: null,
+    load : () => {
+        cart.data = localStorage.getItem('cart');
+        if (cart.data === NULL) {
+            cart.data = {};
+        } else {
+            cart.data === JSON.parse(cart.data);
+        }
+    },
+    save : () => {
+        localStorage.setItem('cart', JSON.stringify(cart.data))
+    },
+    add : () => {
+        if (cart.data[this.dataset.id] === undefined) {
+            let dataSetId = this.dataset.id;
+            let product = products[dataSetId];
+            cart.data[dataSetId];
+            cart.data[dataSetId] = {
+                name : product['name'],
+                img : product['img'],
+                price : product['price'],
+                qty : 1
+            }
+        } else {
+            cart.data[this.dataset.id]['qty']++;
+        }
+        cart.save();
+        cart.list();
+    }
+
+}
 
 const renderProducts = () => {
     window.addEventListener('load', () => {
         const container = document.getElementById('single-product-list');
-        let item, part = null;
-        
+        let item = null, part = null;
+
         for (let i in products) {
             item = document.createElement("div");
+            item.type = "button";
+            item.dataset.id = i;
             item.classList.add("single-product-item")
-
+            item.addEventListener('click', (e) => {
+                cart.add;
+                console.log(e.target)
+            })
+         
             part = document.createElement("img");
             part.src = products[i]['img'];
             part.classList.add("single-product-img");
@@ -65,8 +105,11 @@ const renderProducts = () => {
             part.classList.add('single-product-price');
             item.appendChild(part);
             
-            container.appendChild(part);
+            container.appendChild(item);
+
         }
+
+        
     });
 }
 
@@ -76,3 +119,13 @@ renderProducts();
 // console.log(Object.entries(products))
 // const map = new Map(Object.entries(products));
 // console.log(map)
+
+//function for cleaner code on rendering HTML (Not Working)
+// const renderHTMLElement = (part, element, addClass) => {
+//     part = document.createElement(element);
+//     part.src = products[i][element];
+//     part.classList.add(addClass);
+//     return item.appendChild(part);
+// }
+
+// let size = Object.keys(products).length;
