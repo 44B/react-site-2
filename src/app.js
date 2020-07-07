8f0fbebaec6f20672afff4e8e970aef39e086112
mohur <m.ashrahfi@gmail.com>
2020-07-06 18:33:17 -0700

    finishing cart actions

--- src/app.js
+++ src/app.js
@@ -46,11 +46,8 @@ let cart = {
     data: null,
     load : () => {
         cart.data = localStorage.getItem('cart');
-        if (cart.data === NULL) {
-            cart.data = {};
-        } else {
-            cart.data === JSON.parse(cart.data);
-        }
+        if (cart.data == null) { cart.data = {}; }
+        else { cart.data = JSON.parse(cart.data); }
     },
     save : () => {
         localStorage.setItem('cart', JSON.stringify(cart.data))
@@ -71,9 +68,101 @@ let cart = {
         }
         cart.save();
         cart.list();
-    }
-
-}
+    },
+    list: () => {
+        let container = document.getElementById("cart-info-items");
+        let item = null, part = null, product = null;
+        container.innerHTML = '';
+        let isEmpty = (obj) => {
+        for (var key in obj) {
+          if(obj.hasOwnProperty(key)) { return false; }
+        }
+        return true;
+      };
+      if (isEmpty(cart.data)) {
+        item = document.createElement("div");
+        item.innerHTML = "Cart is empty";
+        container.appendChild(item);
+      } else {
+        // List items
+        let total = 0, subtotal = 0;
+        for (var i in cart.data) {
+          item = document.createElement("div");
+          item.classList.add("c-item");
+          product = cart.data[i];
+  
+          // Quantity
+          part = document.createElement("input");
+          part.type = "number";
+          part.value = product['qty'];
+          part.dataset.id = i;
+          part.classList.add("c-qty");
+          part.addEventListener("change", cart.change);
+          item.appendChild(part);
+  
+          // Name
+          part = document.createElement("span");
+          part.innerHTML = product['name'];
+          part.classList.add("c-name");
+          item.appendChild(part);
+  
+          // Subtotal
+          subtotal = product['qty'] * product['price'];
+          total += subtotal;
+          container.appendChild(item);
+        }
+  
+        // EMPTY BUTTONS
+        item = document.createElement("input");
+        item.type = "button";
+        item.value = "Empty";
+        item.addEventListener("click", cart.reset);
+        item.classList.add("c-empty");
+        container.appendChild(item);
+  
+        // CHECKOUT BUTTONS
+        item = document.createElement("input");
+        item.type = "button";
+        item.value = "Checkout - " + "$" + total;
+        item.addEventListener("click", cart.checkout);
+        item.classList.add("c-checkout");
+        container.appendChild(item);
+      }
+    },
+    change : () => {
+        // change() : change quantity
+        if (this.value == 0) {
+            delete cart.data[this.dataset.id];
+        } else {
+            cart.data[this.dataset.id]['qty'] = this.value;
+        }
+        cart.save();
+        cart.list();
+        },
+      
+        reset : () => {
+        // reset() : empty cart
+            if (confirm("Empty cart?")) {
+                cart.data = {};
+                cart.save();
+                cart.list();
+            } 
+        },
+      
+        checkout : () => {
+        // checkout() : checkout the cart
+      
+          alert("TODO");
+          // Forward to confirmation page or directly add name, tel, email fields in the cart list.
+          // Send cart.data to the server and do further processing - email or save to database.
+        }
+};
+      
+      // Load previous cart and update HTML on load
+    window.addEventListener("load", () => {
+        cart.load();
+        cart.list();
+    });
 
 const renderProducts = () => {
     window.addEventListener('load', () => {
